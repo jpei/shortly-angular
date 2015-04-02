0046080d33c7f52a9b4a84347eecce362e529608
Joseph Pei <josephdpei@gmail.com>
2015-04-02 16:45:05 -0700

    Made custom directive

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -8,12 +8,15 @@ angular.module('shortly.links', [])
 })
 .directive('myShortLink', function() {
   return {
-    // template: "<div class='info'> \
-    //   <div class='visits'><span class='count'>{{link.visits}}</span>Visits</div> \
-    //   <div class='title'>{{link.title}}</div> \
-    //   <div class='original'>{{link.url}}</div> \
-    //   <a href='/api/links/{{link.code}}'>{{link.base_url}}/{{link.code}}</a> \
-    // </div>"
-    template: "<p>5</p>"
+    template: "<div id='container' class='link'> \
+      <img src='../../assets/redirect_icon.png'/> \
+      <div class='info'> \
+      <div class='visits'><span class='count'>{{link.visits}}</span>Visits</div> \
+      <div class='title'>{{link.title}}</div> \
+      <div class='original'>{{link.url}}</div> \
+      <a href='/api/links/{{link.code}}'>{{link.base_url}}/{{link.code}}</a> \
+    </div></div>",
+
+    restrict: 'E'
   };
 });
